ff611fe1862d68b540da7ea2f8b0d5feae54c3ed
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-02-17 14:01:32 +0700

    App layout

---
+++ client/src/components/common/ProtectedPage.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthModalOpen } from "../../app/features/authModelSlice";
+
+const ProtectedPage = ({ children }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  useEffect(() => {
+    dispatch(setAuthModalOpen(!user));
+  }, [dispatch, user]);
+  return children;
+};
+
+export default ProtectedPage;
---
+++ client/src/components/layout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Box display="flex" minHeight="100vh">
+        {/* main */}
+        <Box component="main" flexGrow={1} overflow="hidden" minHeight="100vh">
+          <Outlet />
+        </Box>
+        {/* main */}
+      </Box>
+      {/* footer */}
+      {/* footer */}
+    </>
+  );
+};
+
+export default MainLayout;
