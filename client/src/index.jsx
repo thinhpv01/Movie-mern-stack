d6b4002670dee296348439bbeafc7b9ccdfae1d6
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-02-17 10:23:29 +0700

    💦 Config redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,10 +6,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={{}}>
+    <Provider store={store}>
         <App />
     </Provider>
 );
