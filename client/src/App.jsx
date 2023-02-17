66cbde5735fa38949c899251bb43282a63632159
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-02-17 11:45:39 +0700

    ✏ create config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.config";
+import { ToastContainer } from "react-toastify";
 function App() {
-  return <div>app</div>;
+  const themeMode = useSelector((state) => state.themeMode);
+  return (
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeButton
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      >
+        <CssBaseline />
+      </ToastContainer>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appStateSlice from "./features/appStateSlice";
+import { authModalSlice } from "./features/authModelSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+import { themeModeSlice } from "./features/themeModalSlice";
+import userSlice from "./features/userSlice";
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userSlice,
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
+    appState: appStateSlice,
+  },
 });
 export default store;
