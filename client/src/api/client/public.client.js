a95ffb59b4b598b3d285bac6dc27d91c3c3367d6
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-02-17 09:31:36 +0700

    🔨 Config api service

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -14,7 +14,6 @@ privateClient.interceptors.request.use(async (config) => {
     ...config,
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("actkn")}`,
     },
   };
 });
---
+++ client/src/api/modules/user.api.js
@@ -0,0 +1,66 @@
+import privateClient from "../client/private.client";
+import publicClient from "../client/public.client";
+
+const userEndpoints = {
+  signin: "user/signin",
+  signup: "user/signup",
+  getInfo: "user/info",
+  passwordUpdate: "user/update-password",
+};
+
+const userApi = {
+  signin: async ({ username, password }) => {
+    try {
+      console.log("send request");
+      const response = await publicClient.post(userEndpoints.signin, {
+        username,
+        password,
+      });
+      return { response };
+    } catch (err) {
+      console.log("err");
+      return { err };
+    }
+  },
+  signup: async ({ username, password, confirmPassword, displayName }) => {
+    try {
+      console.log("send request");
+      const response = await publicClient.post(userEndpoints.signup, {
+        username,
+        password,
+        confirmPassword,
+        displayName,
+      });
+      return { response };
+    } catch (err) {
+      console.log("err");
+      return { err };
+    }
+  },
+  getInfo: async () => {
+    try {
+      console.log("send request");
+      const response = await privateClient.post(userEndpoints.getInfo);
+      return { response };
+    } catch (err) {
+      console.log("err");
+      return { err };
+    }
+  },
+  passwordUpdate: async ({ password, newPassword, confirmNewPassword }) => {
+    try {
+      console.log("send request");
+      const response = await privateClient.post(userEndpoints.passwordUpdate, {
+        password,
+        newPassword,
+        confirmNewPassword,
+      });
+      return { response };
+    } catch (err) {
+      console.log("err");
+      return { err };
+    }
+  },
+};
+
+export default userApi;
