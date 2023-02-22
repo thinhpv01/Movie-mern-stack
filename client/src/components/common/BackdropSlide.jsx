bdbf90c2e4a12c74c7bcfcc162b2937070d975f1
Thinh Pham-Van <phamvanthinh2001x@gmail.com>
2023-02-22 08:59:44 +0700

    ✏ Create slide

--- client/src/components/common/BackdropSlide.jsx
+++ client/src/components/common/BackdropSlide.jsx
@@ -1,7 +1,28 @@
+import { Box } from "@mui/system";
 import React from "react";
+import { SwiperSlide } from "swiper/react";
+import NavigationSwiper from "./NavigationSwiper";
+import tmdbConfigs from "../../api/configs/tmdb.configs";
 
-const BackdropSlide = () => {
-  return <div>BackdropSlide</div>;
+const BackdropSlide = ({ backdrops }) => {
+  return (
+    <NavigationSwiper>
+      {[...backdrops].splice(0, 10).map((item, index) => (
+        <SwiperSlide key={index}>
+          <Box
+            sx={{
+              paddingTop: "60%",
+              backgroundPosition: "top",
+              backgroundSize: "cover",
+              backgroundImage: `url(${tmdbConfigs.backdropPath(
+                item.file_path
+              )})`,
+            }}
+          />
+        </SwiperSlide>
+      ))}
+    </NavigationSwiper>
+  );
 };
 
 export default BackdropSlide;
--- client/src/components/common/RecommendSlide.jsx
+++ client/src/components/common/RecommendSlide.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { SwiperSlide } from "swiper/react";
+import AutoSwiper from "./AutoSwiper";
+import MediaItem from "./MediaItem";
 
-const RecommendSlide = () => {
-  return <div>RecommendSlide</div>;
+const RecommendSlide = ({ medias, mediaType }) => {
+  return (
+    <AutoSwiper>
+      {medias.map((media, index) => (
+        <SwiperSlide key={index}>
+          <MediaItem media={media} mediaType={mediaType} />
+        </SwiperSlide>
+      ))}
+    </AutoSwiper>
+  );
 };
 
 export default RecommendSlide;
